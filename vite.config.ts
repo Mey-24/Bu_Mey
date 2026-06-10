9a95fcf2c92db1e23b5c7b1f8c0b0cf00757b4eb
Mey-24 <nia.sarigadung2@gmail.com>
2026-06-10 21:52:59 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: base: '/Bu_Mey/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
